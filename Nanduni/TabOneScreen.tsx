847a4f26481f78252f88414e450f1d9a17d6fa6d
nadzmihisaree <66440914+nadzmihisaree@users.noreply.github.com>
2021-03-25 23:59:02 +0530

    homeNew

--- Nanduni/TabOneScreen.tsx
+++ Nanduni/TabOneScreen.tsx
@@ -1,342 +1,115 @@
 import * as React from 'react';
-import { ImageBackground,StyleSheet,Image} from 'react-native';
+import { ImageBackground, Text, View, StyleSheet,Image,TouchableOpacity} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { SafeAreaView } from 'react-native';
+// //import EditScreenInfo from '../components/EditScreenInfo';
+// import { Text, View } from '../components/Themed';
+//import { SafeAreaView } from 'react-native';
 import { Card,Button, Icon } from 'react-native-elements';
+import HomeCard from '../screens/home/HomeCard';
 
-// const image = { uri: "https://reactjs.org/logo-og.png" };
-const App = () =>{
+
+
+const App = () =>{ 
 
 return (
-  <View>
-    <View style={styles.container}>
-    <Image source={require('../components/images/pic3.jpg')} style={styles.backgroundImage} />
-    </View>
+ 
 
-      <View style={{backgroundColor:'white'}}>
+    <View>
+
+      <View style={{ backgroundColor: 'white' }}>
         <ScrollView>
 
-          <Text style={styles.innerText}>  Italian</Text>
-    
+         {/* <View> */}
+         {/* <Image source={require('../components/images/pic3.jpg')}style={{ width:'100%', height: 250}} /> */}
+         {/* <TouchableOpacity>
+         <ImageBackground source={require('../components/images/pic3.jpg')}style={{ width:'100%', height: 250}} >
+            <Text > spaghetti  </Text> 
+          </ImageBackground>
+          </TouchableOpacity>
 
-{/* -------------------------------------------------------------------------------------------------------------------------------- */}
-            <ScrollView horizontal={true}>
-
-              <Card>
-              {/* <Card.Title>HELLO WORLD</Card.Title> */}
-              {/* <Card.Divider/> */}
-              <Card.Image source={require('../components/images/food1.jpg')}>
-                {/* <Text style={{marginBottom: 10}}>
-                    The idea with React Native Elements is more about component structure than actual design.
-                </Text> */}
-                {/* <Button
-                icon={<Icon name='code' color='#ffffff' />}
-                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 60}}
-                title='spaghetti' /> */}
-              </Card.Image>
-              <Button
-                icon={<Icon name='code' color='#ffffff'  />}
-                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, backgroundColor:'black',}}
-                title ='spaghetti'  />
-              </Card>
-
-              <View style={{backgroundColor:'white',padding:20, }}>
-              <Card title="Local Modules">
-              <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                <Text style = {{color:'black'}}>
-                spaghetti
-                </Text>
-              </Card>
-              </View>
-
-              <View style={{backgroundColor:'white',padding:20,}}>
-              <Card title="Local Modules">
-              <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                <Text style = {{color:'black'}}>
-                spaghetti
-                </Text>
-              </Card>
-              </View>
-
-              <View style={{backgroundColor:'white',padding:20, }}>
-              <Card title="Local Modules">
-              <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                <Text style = {{color:'black'}}>
-                spaghetti
-                </Text>
-              </Card>
-              </View>
-
-              <View style={{backgroundColor:'white',padding:20, }}>
-              <Card title="Local Modules">
-              <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                <Text style = {{color:'black'}}>
-                spaghetti
-                </Text>
-              </Card>
-              </View>
-
-              <View style={{backgroundColor:'white',padding:20, }}>
-              <Card title="Local Modules">
-              <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                <Text style = {{color:'black'}}>
-                spaghetti
-                </Text>
-              </Card>
-              </View>
-
-            </ScrollView>
-
-{/* ------------------------------------------------------------ */}
+         
+         </View> */}
           
-            <Text style={styles.innerText}>  Italian</Text>
-    
+          <TouchableOpacity>
+        <ImageBackground
+          style={styles.backgroundImage}
+          // imageStyle={{ borderRadius: theme.borders_MediumRadius.borderRadius, backgroundColor: 'rgba(255,0,0,.6)' }}
+          source={require('../components/images/pic3.jpg')}
+        >
+            <Text style={styles.bgtext} >{"\n"}{"\n"}   Search recipies by {"\n"}   ingredients</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+         
 
-            <ScrollView horizontal={true}>
-                
-              <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-              </ScrollView>
-              
-{/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
-
-            <Text style={styles.innerText}>  Italian</Text>
-    
-
-              <ScrollView horizontal={true}>
-          
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-              </ScrollView>
-          
-{/* -------------------------------------------------------------------------------------------------------         */}
-          
-          <Text style={styles.innerText}>  Italian</Text>
-    
-
-            <ScrollView horizontal={true}>
-                
-              <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
-
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
-
-              </ScrollView>
-
-{/* ------------------------------------------------------------ */}
           <Text style={styles.innerText}>  Italian</Text>
 
-            <ScrollView horizontal={true}>
-                
-              <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
 
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
+          {/* -------------------------------------------------------------------------------------------------------------------------------- */}
+          <ScrollView horizontal={true}>
 
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
 
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
 
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
+          </ScrollView>
 
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
+          {/* ------------------------------------------------------------ */}
 
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
+          <Text style={styles.innerText}>  Italian</Text>
 
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
 
-                <View style={{backgroundColor:'white',padding:20, }}>
-                <Card title="Local Modules">
-                <Image source={require('../components/images/food1.jpg')} style={{ width: 100, height:100}}></Image>
+          <ScrollView horizontal={true}>
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+          </ScrollView>
 
-                  <Text style = {{color:'black'}}>
-                  spaghetti
-                  </Text>
-                </Card>
-                </View>
+          {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
 
-              </ScrollView>
-      
-    
-      </ScrollView>
+          <Text style={styles.innerText}>  Italian</Text>
+
+
+          <ScrollView horizontal={true}>
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+          </ScrollView>
+
+          {/* -------------------------------------------------------------------------------------------------------         */}
+
+          <Text style={styles.innerText}>  Italian</Text>
+
+
+          <ScrollView horizontal={true}>
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+          </ScrollView>
+
+          {/* ------------------------------------------------------------ */}
+          <Text style={styles.innerText}>  Italian</Text>
+
+          <ScrollView horizontal={true}>
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+            <HomeCard />
+          </ScrollView>
+
+        </ScrollView>
+      </View>
     </View>
-
-   
-      
-      
-  </View>
     
   );
 };
@@ -360,9 +133,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 20,
     backgroundColor: '#fff',
-   
-    
+    flexDirection: 'column',   
   },
+
+
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -378,11 +152,55 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',  
   },
+  // backgroundImage: {
+  //   width:'100%',
+  //   height:500,
+  //   // flex: 1,
+  //   // resizeMode: 'cover', // or 'stretch'
+  // } 
+
+
+
+
+  // container: {
+  //   flex: 1,
+  //   flexDirection: 'column',
+  // },
+  // image: {
+  //   flex: 1,
+  //   resizeMode: 'cover',
+  //   justifyContent: 'center',
+  //   height:'40%',
+  //   width: '100%',
+  // },
+  // text: {
+  //   color: 'grey',
+  //   fontSize: 30,
+  //   fontWeight: 'bold',
+  // },
+
   backgroundImage: {
-    width:'100%',
-    height:500,
-    // flex: 1,
-    // resizeMode: 'cover', // or 'stretch'
-  } 
+    flex: 1,
+    width: '100%',
+    height: 200,
+},
+
+bgtext:{
+  fontSize:20,
+  color: 'white',
+  
+},
+
+btnText:{
+  fontSize: 15,
+  color: 'black',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  backgroundColor: 'white',
+//   borderRadius: 10,
+//   borderWidth: 2,
+  paddingBottom: 2,
+  paddingTop: 2,
+}
   
 });
