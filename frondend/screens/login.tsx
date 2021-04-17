a21841434d972328b789b12d391d1192bd165ee4
chathushika2000 <66440838+chathushika2000@users.noreply.github.com>
2021-04-18 00:01:50 +0530

    updated

--- frondend/screens/login.tsx
+++ frondend/screens/login.tsx
@@ -7,15 +7,20 @@ import { Text, View } from '../components/Themed';
 import { RootStackParamList } from '../types';
 
 export default function register ({navigation,} : StackScreenProps<RootStackParamList, 'Root'>) {
-  
+  const image = { uri: "https://media.istockphoto.com/vectors/preparations-and-mushroom-dishes-on-a-white-background-vector-id1264636364?s=612x612" };
    return(
     <View style={styles.container}>
+      <ImageBackground source ={require('../components/Images/i4.jpg')} style={styles.image}>
+      <View style={{marginLeft:40,marginRight:40,marginTop:220,marginBottom:120,backgroundColor:'white',borderRadius:30,borderWidth:0,}}>
+
+      <View>
       <Text style={{padding:10, fontWeight:'bold',fontSize:35,marginTop:30,color:'black',textAlign:'center'}}>Login</Text>
+      </View>
       <TextInput
         style={styles.input}
         placeholder='User Name :'
         autoCapitalize="none"
-        placeholderTextColor='white'
+        placeholderTextColor='#a9a9a9'
       />
 
       <TextInput
@@ -23,18 +28,24 @@ export default function register ({navigation,} : StackScreenProps<RootStackPara
           placeholder='Password :'
           secureTextEntry={true}
           autoCapitalize="none"
-          placeholderTextColor='white'
+          placeholderTextColor='#a9a9a9'
         />
-       <View style={{marginTop:50,padding:15,borderRadius: 20}}>
+       <View style={{marginTop:20,padding:15,borderRadius: 20}}>
       <TouchableOpacity >
-      <Button
+      {/* <Button
         title='  Login  '
         onPress={()=> navigation.navigate('Root')}
         color={'#407294'}
-      />
+      /> */}
+      <TouchableOpacity  onPress={()=> navigation.navigate('Root')}>
+        <Text style={{padding:12,fontWeight:'bold',fontSize:25,marginTop:0,color:'white',backgroundColor:'#fbb124',textAlign:'center',borderRadius:50}}>Login</Text>
+
+        </TouchableOpacity>
+
       </TouchableOpacity>
       </View>
-        
+      </View>
+      </ImageBackground>
     </View>
     
   );
@@ -43,21 +54,27 @@ export default function register ({navigation,} : StackScreenProps<RootStackPara
 
 // }
 const styles = StyleSheet.create({
-input: {
-  width: 350,
-  height: 55,
-  backgroundColor: 'gray',
-  marginTop: 30,
-  marginLeft:15,
-  padding: 8,
-  color: 'black',
-  borderRadius: 14,
-  fontSize: 18,
-  fontWeight: '500',
-},
+  input: {
+    width: 280,
+    height: 55,
+    backgroundColor: 'white',
+    marginTop: 30,
+    marginLeft:15,
+    padding: 8,
+    color: 'black',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '300',
+    borderWidth:1,
+    borderColor:'#fbb124'
+  },
 container: {
   flex: 1,
   justifyContent: 'center',
  
-}
+},
+image: {
+  flex: 1,
+  resizeMode: 'cover',
+},
 });
--- frondend/screens/register.tsx
+++ frondend/screens/register.tsx
@@ -1,4 +1,4 @@
-import {Button,TextInput,StyleSheet,Image} from 'react-native'
+import {Button,TextInput,StyleSheet,ImageBackground} from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import * as React from 'react';
 import  { Component } from "react";
@@ -8,83 +8,91 @@ import { Text, View } from '../components/Themed';
   import { RootStackParamList } from '../types';
 
 export default function register ({navigation,} : StackScreenProps<RootStackParamList, 'Root'>){
-  
+  const image = { uri: "https://media.istockphoto.com/vectors/preparations-and-mushroom-dishes-on-a-white-background-vector-id1264636364?s=612x612" };
     return (
+      
       <View >
+        
         <ScrollView>
-        
-        <View>
+        <ImageBackground source ={require('../components/Images/i4.jpg')} style={styles.image}>
+        <View style={{marginLeft:40,marginRight:40,marginTop:120,marginBottom:120,backgroundColor:'white',borderRadius:30,borderWidth:0,}}>
+          <View>
         <Text style={{padding:10, fontWeight:'bold',fontSize:35,marginTop:30,color:'black',textAlign:'center'}}>Register Page</Text>
-        {/* <Image source={require('../components/Images/reg.jpg')} style={{width:'100%', height: 250}}></Image>  */}
-        
         </View>
       <View style={styles.container}>
         <TextInput
           style={styles.input}
           placeholder='First Name :'
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('first_name', val)}
+          placeholderTextColor='#a9a9a9'
+          
         />
 
         <TextInput
-          style={styles.input}
+          style={styles.input} 
           placeholder='Last Name :'
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('last_name', val)}
+          placeholderTextColor='#a9a9a9'
+          
         />
 
         <TextInput
           style={styles.input}
           placeholder='Username :'
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('username', val)}
+          placeholderTextColor='#a9a9a9'
+          
         />
         <TextInput
           style={styles.input}
           placeholder='Password :'
           secureTextEntry={true}
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('password', val)}
+          placeholderTextColor='#a9a9a9'
+         
         />
          <TextInput
           style={styles.input}
           placeholder='Confirm Password :'
           secureTextEntry={true}
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('Confirm_password', val)}
+          placeholderTextColor='#a9a9a9'
+         
         />
 
         <TextInput
           style={styles.input}
           placeholder='Email :'
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('email', val)}
+          placeholderTextColor='#a9a9a9'
+          
         />
         <TextInput
           style={styles.input}
           placeholder='Phone Number :'
           autoCapitalize="none"
-          placeholderTextColor='white'
-          // onChangeText={val => this.onChangeText('phone_number', val)}
+          placeholderTextColor='#a9a9a9'
+          
         />
         </View>
-        <View style={{marginTop:50,padding:15,borderRadius:50}}>
-          <TouchableOpacity >
-        <Button
+        <View style={{marginTop:30,padding:0,borderRadius:50,marginLeft:20,marginRight:20}}>
+          
+        {/* <Button
           title='  Register  '
           onPress={()=> navigation.navigate('Root')} 
-          color={'#407294'}
-        />
+          color={'#fbb124'}
+      
+        /> */}
+        <TouchableOpacity  onPress={()=> navigation.navigate('Root')}>
+        <Text style={{padding:12,fontWeight:'bold',fontSize:25,marginTop:'5%',color:'white',backgroundColor:'#fbb124',textAlign:'center',borderRadius:50}}>Register</Text>
+
         </TouchableOpacity>
         </View>
-        <Text style={{padding:10,fontWeight:'bold',fontSize:3,marginTop:'20%',color:'white'}}>magic Wok</Text>
+        <Text style={{padding:10,fontWeight:'bold',fontSize:3,marginTop:5,color:'white'}}>magic Wok</Text>
+        </View>
+        </ImageBackground>
         </ScrollView>
+        
       </View>
     
   
@@ -95,21 +103,27 @@ export default function register ({navigation,} : StackScreenProps<RootStackPara
 
 const styles = StyleSheet.create({
   input: {
-    width: 350,
+    width: 280,
     height: 55,
-    backgroundColor: 'gray',
+    backgroundColor: 'white',
     marginTop: 30,
     marginLeft:15,
     padding: 8,
     color: 'black',
     borderRadius: 14,
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: '300',
+    borderWidth:1,
+    borderColor:'#fbb124'
   },
   container: {
     flex: 1,
     justifyContent: 'center',
    
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
   },
 });
 
