e492d482f5e85d428598463d37212ca576ee3b73
nadzmihisaree <66440914+nadzmihisaree@users.noreply.github.com>
2021-04-18 22:12:10 +0530

    updated

--- frondend/screens/TabOneScreen.tsx
+++ frondend/screens/TabOneScreen.tsx
@@ -1,124 +1,252 @@
-import * as React from 'react';
-import { ImageBackground, Text, View, StyleSheet,Image,TouchableOpacity} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
 
-// //import EditScreenInfo from '../components/EditScreenInfo';
-// import { Text, View } from '../components/Themed';
-//import { SafeAreaView } from 'react-native';
+import React, { Component } from 'react'
+import { Animated, View, StyleSheet, Image, Dimensions, ScrollView , Text,SafeAreaView} from 'react-native'
 import { Card,Button, Icon } from 'react-native-elements';
 import HomeCard from '../screens/favorite/HomeCard';
 
+const deviceWidth = Dimensions.get('window').width
+const FIXED_BAR_WIDTH = 280
+const BAR_SPACE = 10
 
+const images = [
+  'https://st2.depositphotos.com/2576363/6461/i/600/depositphotos_64612467-stock-photo-ingredients-for-baking-on-black.jpg',
+  'https://hintwallaper.com/wp-content/uploads/2020/11/Food-Background-HD-1024x654.jpg',
+  
+]
 
-const App = () =>{ 
+export default class extends Component {
 
-return (
- 
+  numItems = images.length
+  itemWidth = (FIXED_BAR_WIDTH / this.numItems) - ((this.numItems - 1) * BAR_SPACE)
+  animVal = new Animated.Value(0)
 
-    <View>
+  render() {
+    let imageArray = []
+    let barArray = []
+    images.forEach((image, i) => {
+      console.log(image, i)
+      const thisImage = (
+        <Image
+          key={`image${i}`}
+          source={{uri: image}}
+          style={{ width: deviceWidth , height: '100%' }}
+        />
+        
+      )
+      
+      imageArray.push(thisImage)
 
-      <View style={{ backgroundColor: 'white' }}>
-        <ScrollView>
+      const scrollBarVal = this.animVal.interpolate({
+        inputRange: [deviceWidth * (i - 1), deviceWidth * (i + 1)],
+        outputRange: [-this.itemWidth, this.itemWidth],
+        extrapolate: 'clamp',
+      })
 
-         {/* <View> */}
-         {/* <Image source={require('../components/images/pic3.jpg')}style={{ width:'100%', height: 250}} /> */}
-         {/* <TouchableOpacity>
-         <ImageBackground source={require('../components/images/pic3.jpg')}style={{ width:'100%', height: 250}} >
-            <Text > spaghetti  </Text> 
-          </ImageBackground>
-          </TouchableOpacity>
-
-         
-         </View> */}
-          
-          <TouchableOpacity>
-        <ImageBackground
-          style={styles.backgroundImage}
-          // imageStyle={{ borderRadius: theme.borders_MediumRadius.borderRadius, backgroundColor: 'rgba(255,0,0,.6)' }}
-          source={require('../components/Images/pic3.jpg')}
+      const thisBar = (
+        <View
+          key={`bar${i}`}
+          style={[
+            styles.track,
+            {
+              width: this.itemWidth,
+              marginLeft: i === 0 ? 0 : BAR_SPACE,
+            },
+          ]}
         >
-            <Text style={styles.bgtext} >{"\n"}{"\n"}   Search recipies by {"\n"}   ingredients</Text>
-        </ImageBackground>
-      </TouchableOpacity>
+          <Animated.View
+
+            style={[
+              styles.bar,
+              {
+                width: this.itemWidth,
+                transform: [
+                  { translateX: scrollBarVal },
+                ],
+              },
+            ]}
+          />
+        </View>
+      )
+      barArray.push(thisBar)
+    })
+
+    return (
+      <SafeAreaView>
+          <View style={styles.container} >
+             
+
+          
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              scrollEventThrottle={10}
+              pagingEnabled
+              onScroll={
+                Animated.event(
+                  [{ nativeEvent: { contentOffset: { x: this.animVal } } }]
+                )
+              }
+            >
+
+              {imageArray}
+              
+              <View style={styles.skip}>
+                    <Text style={{color:"yellow",textAlign:"center",fontSize:16}}>Search{"\n"} recipes by{"\n"}ingredients</Text>
+              </View>
+
+            </ScrollView>
+
+            <View
+              style={styles.skip1}>
+                    <Text style={{color:"yellow",textAlign:"center",fontSize:16}}>Search{"\n"} recipes by{"\n"}dish</Text>
+              </View>
+
+            <View
+              style={styles.barContainer}
+            >
+              {barArray}
+            </View>
+          </View>
+
+
+          <View style ={styles.container1}>
+            <ScrollView>
+                
+                <Text style={styles.innerText}>  Italian</Text>
+
+
+                {/* -------------------------------------------------------------------------------------------------------------------------------- */}
+                <ScrollView horizontal={true}>
+
+
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+
+                </ScrollView>
+
+                {/* ------------------------------------------------------------ */}
+
+                <Text style={styles.innerText}>  Italian</Text>
+
+
+                <ScrollView horizontal={true}>
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                </ScrollView>
+
+                {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
+
+                <Text style={styles.innerText}>  Italian</Text>
+
+
+                <ScrollView horizontal={true}>
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                </ScrollView>
+
+                {/* -------------------------------------------------------------------------------------------------------         */}
+
+                <Text style={styles.innerText}>  Italian</Text>
+
+
+                <ScrollView horizontal={true}>
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                </ScrollView>
+
+                {/* ------------------------------------------------------------ */}
+                <Text style={styles.innerText}>  Italian</Text>
+
+                <ScrollView horizontal={true}>
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                  <HomeCard />
+                </ScrollView>
+
+              </ScrollView>
+            
+          </View>
          
 
-          <Text style={styles.innerText}>  Italian</Text>
-
-
-          {/* -------------------------------------------------------------------------------------------------------------------------------- */}
-          <ScrollView horizontal={true}>
-
-
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-
-          </ScrollView>
-
-          {/* ------------------------------------------------------------ */}
-
-          <Text style={styles.innerText}>  Italian</Text>
-
-
-          <ScrollView horizontal={true}>
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-          </ScrollView>
-
-          {/* ---------------------------------------------------------------------------------------------------------------------------------------- */}
-
-          <Text style={styles.innerText}>  Italian</Text>
-
-
-          <ScrollView horizontal={true}>
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-          </ScrollView>
-
-          {/* -------------------------------------------------------------------------------------------------------         */}
-
-          <Text style={styles.innerText}>  Italian</Text>
-
-
-          <ScrollView horizontal={true}>
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-          </ScrollView>
-
-          {/* ------------------------------------------------------------ */}
-          <Text style={styles.innerText}>  Italian</Text>
-
-          <ScrollView horizontal={true}>
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-            <HomeCard />
-          </ScrollView>
-
-        </ScrollView>
-      </View>
-    </View>
-    
-  );
-};
-export default App;
-
+      </SafeAreaView>
+         
+    )
+  }
+}
 
 
 
 const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '50%',
+    // marginTop: '30%',
+    marginBottom: '-65%',
+  },
+  barContainer: {
+    position: 'absolute',
+    zIndex: 2,
+    bottom: '25%',
+    flexDirection: 'row',
+  },
+
+
+
+    skip: {
+    position: 'absolute',
+    //zIndex: 1,
+    bottom: '60%',
+    flexDirection: 'row',
+
+  },
+
+  skip1: {
+    // position: 'absolute',
+    //zIndex:0,
+    bottom: '60%',
+    flexDirection: 'row',
+
+  },
+
+
+
+  track: {
+    backgroundColor: '#ccc',
+    overflow: 'hidden',
+    height: 2,
+  },
+  bar: {
+    backgroundColor: '#5294d6',
+    //backgroundColor: '#00FFFF',
+    height: 2,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+
+
+
+
+
+
+
 
   innerText: {
     color: '#C70039',
@@ -127,13 +255,15 @@ const styles = StyleSheet.create({
 
   },
 
-  container: {
-    flex: 1,
-    alignItems:'flex-start',
+  container1: {
+    // flex: 1,
+    // alignItems:'flex-start',
     justifyContent: 'center',
     paddingTop: 20,
     backgroundColor: '#fff',
-    flexDirection: 'column',   
+    flexDirection: 'column',  
+    marginTop: '40%',
+    marginBottom: '30%',
   },
 
 
@@ -152,32 +282,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',  
   },
-  // backgroundImage: {
-  //   width:'100%',
-  //   height:500,
-  //   // flex: 1,
-  //   // resizeMode: 'cover', // or 'stretch'
-  // } 
 
-
-
-
-  // container: {
-  //   flex: 1,
-  //   flexDirection: 'column',
-  // },
-  // image: {
-  //   flex: 1,
-  //   resizeMode: 'cover',
-  //   justifyContent: 'center',
-  //   height:'40%',
-  //   width: '100%',
-  // },
-  // text: {
-  //   color: 'grey',
-  //   fontSize: 30,
-  //   fontWeight: 'bold',
-  // },
 
   backgroundImage: {
     flex: 1,
@@ -197,10 +302,9 @@ btnText:{
   fontWeight: 'bold',
   textAlign: 'center',
   backgroundColor: 'white',
-//   borderRadius: 10,
-//   borderWidth: 2,
+
   paddingBottom: 2,
   paddingTop: 2,
 }
   
-});
+})
