d2401ba8168bcd0a39f8929378c538384a4df57c
chathushika2000 <66440838+chathushika2000@users.noreply.github.com>
2021-04-21 12:40:18 +0530

    updated

--- frondend/screens/about.tsx
+++ frondend/screens/about.tsx
@@ -1,18 +1,28 @@
 import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Button,Image} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import { StackScreenProps } from '@react-navigation/stack';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { RootStackParamList } from '../types';
 
-export default function about() {
+export default function about({navigation,}: StackScreenProps<RootStackParamList, 'Setting'>) {
 
   return (
     <View> 
       <ScrollView>
-    <Image source={require('../components/Images/fo1.jpg')} style={{width:'110%', height: 250,marginLeft:-10}}></Image> 
+      <View>
+         <TouchableOpacity onPress={()=> navigation.navigate('Setting')}>
+          <Image source={require('../components/Images/k1.png')} style={{width:30, height:30, marginLeft:'5%',marginTop:30}}></Image> 
+          </TouchableOpacity>
+          </View>
+      <View>
+       <Image source={require('../components/Images/i6.jpg')} style={{width:'100%', height: 250,marginTop:10}}></Image> 
+       </View>
+      
     <View>
-    <Text style={{padding:10, fontWeight:'bold',fontSize:25,marginTop:-150,color:'white',marginLeft:'40%'}}>About</Text>
+    <Text style={{padding:10, fontWeight:'bold',fontSize:35,marginTop:-170,color:'black',textAlign:'center'}}>A  b  o  u  t  </Text>
     </View>
     <View>
     <Text style={{padding:5, fontSize:12,marginTop:30}}>qwedrfgthjklsedrftghyujkledrfgthjk    rfghj swdfgh sdfgh sdfghj sddfgh   dfgh    ghjk  hyjk  ghjk
