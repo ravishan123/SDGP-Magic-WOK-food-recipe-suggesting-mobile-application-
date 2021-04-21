513cc7c0535b69646b20736386313e77853787a8
chathushika2000 <66440838+chathushika2000@users.noreply.github.com>
2021-04-21 13:54:23 +0530

    updated

--- frondend/screens/SettingScreen.tsx
+++ frondend/screens/SettingScreen.tsx
@@ -6,12 +6,16 @@
   import { Text, View } from '../components/Themed';
   import { RootStackParamList } from '../types';
   
-  export default function SettingsScreen({navigation,}: StackScreenProps<RootStackParamList, 'help'>) {
-    // function SettingsScreen({navigation,}: StackScreenProps<RootStackParamList, 'about'>) {
+  export default function SettingsScreen({navigation,}: StackScreenProps<RootStackParamList, 'Root'>) {
+   
     return (
       <View>
-        {/* <ImageBackground source={require('../components/images/sett1.jpg')} style={{width:'100%' , height:250}}></ImageBackground>   */}
-          <Text style={{padding:10, fontWeight:'bold',fontSize:25,marginTop:20,color:'black'}}>Settings</Text>
+         <View>
+         <TouchableOpacity onPress={()=> navigation.navigate('Root')}>
+          <Image source={require('../components/Images/k1.png')} style={{width:30, height:30, marginLeft:'5%',marginTop:60}}></Image> 
+          </TouchableOpacity>
+          </View>
+          <Text style={{padding:10, fontWeight:'bold',fontSize:25,marginTop:-20,color:'black',textAlign:'center'}}>Settings</Text>
           <Text style={{padding:10, fontWeight:'bold',fontSize:20,marginTop:20 ,color:'black'}}>Profile</Text>
             <TouchableOpacity  onPress={()=> navigation.navigate('accountSetting')}>
           <Text style={{padding:15, fontWeight:'bold',fontSize:15,marginTop:25,color:'black',backgroundColor:'#fff',borderRadius:10,shadowOpacity:150,elevation:25}}>Account Settings</Text>
@@ -32,7 +36,7 @@
           <View>
           <Image source={require('../components/Images/s4.png')} style={{width:30, height:30, marginLeft:'80%',marginTop:-40}}></Image> 
           </View>
-          <TouchableOpacity onPress={()=> navigation.navigate('login')}>
+          <TouchableOpacity onPress={()=> navigation.navigate('Open')}>
           <Text style={{padding:15, fontWeight:'bold',fontSize:15,marginTop:25,color:'black',backgroundColor:'#fff',borderRadius:10,shadowOpacity:150,elevation:25}}>LogOut</Text>
           </TouchableOpacity>
           <View>
