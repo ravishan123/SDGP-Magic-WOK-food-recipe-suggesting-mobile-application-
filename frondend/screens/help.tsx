5a213e69b45b129cab0cd0be9ffef74afd753f90
chathushika2000 <66440838+chathushika2000@users.noreply.github.com>
2021-04-21 14:11:34 +0530

    updated

--- frondend/screens/help.tsx
+++ frondend/screens/help.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Button,Image} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import { RootStackParamList } from '../types';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { StackScreenProps } from '@react-navigation/stack';
 
-export default function help() {
+export default function help({navigation,}: StackScreenProps<RootStackParamList, 'Setting'>) {
 
   return (
     <View> 
       <ScrollView>
-    <Image source={require('../components/Images/fo1.jpg')} style={{width:'110%', height: 250,marginLeft:-10}}></Image> 
+      <View>
+         <TouchableOpacity onPress={()=> navigation.navigate('Setting')}>
+          <Image source={require('../components/Images/k1.png')} style={{width:30, height:30, marginLeft:'5%',marginTop:30}}></Image> 
+          </TouchableOpacity>
+          </View>
+      <View>
+       <Image source={require('../components/Images/i6.jpg')} style={{width:'100%', height: 250,marginTop:10}}></Image> 
+       </View> 
     <View>
-    <Text style={{padding:10, fontWeight:'bold',fontSize:25,marginTop:-150,color:'white',marginLeft:'30%'}}>H    e    l     p</Text>
+    <Text style={{padding:10, fontWeight:'bold',fontSize:35,marginTop:-170,color:'black',textAlign:'center'}}>H    e    l     p</Text>
     </View>
     <View>
     <Text style={{padding:5, fontSize:12,marginTop:30}}>When developing your food delivery marketing plan, your first step is to define who you will be targeting. 
